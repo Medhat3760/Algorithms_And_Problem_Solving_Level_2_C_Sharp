0c7238dbc58475412685410fa2376bd4a8a7749b
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-06 02:48:55 +0200

    Print Reverse Digits

---
+++ Problem5/Problem5.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem5
+{
+    internal class Problem5
+    {
+
+        // Problem #5       (Print Reverse Digits)
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+            } while (Num <= 0);
+            return Num;
+
+        }
+
+        static void ReverseDigits(int Number)
+        {
+
+            int Remainder;
+            while (Number > 0)
+            {
+                Remainder = Number % 10;
+                Number /= 10;
+                Console.WriteLine(Remainder);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            ReverseDigits(ReadPositiveNumbers("Enter a Positive Number?"));
+
+        }
+    }
+}
