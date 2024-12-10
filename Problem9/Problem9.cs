afe605a73e33b0c4921e57faeca244d8d9f39df0
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-10 08:05:44 +0200

    All Digits Frequency)

---
+++ Problem9/Problem9.cs
@@ -0,0 +1,76 @@
+﻿namespace Problem9
+{
+    internal class Problem9
+    {
+
+        // Problem #9       (All Digits Frequency)
+        static int ReadPositiveNumber(string Message)
+        {
+
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+
+            return Num;
+
+        }
+
+        static int CountDigitFrequancy(short DigitToCheck, int Number)
+        {
+
+            int Remainder = 0, FreqCount = 0;
+
+            while (Number > 0)
+            {
+
+                Remainder = Number % 10;
+                Number /= 10;
+
+                if (DigitToCheck == Remainder)
+                {
+
+                    FreqCount++;
+
+                }
+
+            }
+
+            return FreqCount;
+
+        }
+
+        static void PrintAllDigitsFrequency(int Number)
+        {
+
+            int DigitFrequency = 0;
+
+            for (short i = 0; i <= 9; i++)
+            {
+
+                DigitFrequency = CountDigitFrequancy(i, Number);
+
+                if (DigitFrequency > 0)
+                {
+
+                    Console.WriteLine("Digit " + i + " Frequency is " + DigitFrequency + " Time(s)");
+
+                }
+
+            }
+
+        }
+        
+
+        static void Main(string[] args)
+        {
+
+            PrintAllDigitsFrequency(ReadPositiveNumber("Enter The Main Number?"));
+
+        }
+    }
+}
