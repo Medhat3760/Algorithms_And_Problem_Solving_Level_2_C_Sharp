315f2ceedde4ec847a94095c5f8664b35416bbf7
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2024-12-27 22:45:24 +0200

    Check Palindrome Number

---
+++ Problem11/Problem11.cs
@@ -0,0 +1,65 @@
+﻿namespace Problem11
+{
+    // Check Palindrome Number
+    internal class Problem11
+    {
+
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+            } while (Num <= 0);
+            return Num;
+
+        }
+
+        static int ReverseNumber(int Number)
+        {
+
+            int Remainder = 0, Number2 = 0;
+            while (Number > 0)
+            {
+
+                Remainder = Number % 10;
+                Number /= 10;
+                Number2 = Number2 * 10 + Remainder;
+
+            }
+
+            return Number2;
+
+        }
+
+        static bool isPalindromNumber(int Number)
+        {
+
+            return Number == ReverseNumber(Number);
+
+        }
+
+        static void PrintResult(int Number)
+        {
+
+            if (isPalindromNumber(Number))
+            {
+                Console.WriteLine("\n yes, its a Palindrome Number");
+            }
+            else
+            {
+                Console.WriteLine("\n NO, its Not a Palindrome Number");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintResult(ReadPositiveNumbers("Enter The Number that You Want to Check if its Palindrom?"));
+
+        }
+    }
+}
