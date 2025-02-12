109431e4b92af6dd11da642311b237b84d487b4d
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-13 01:33:31 +0200

    Reverse Number

---
+++ Problem12/Problem12.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem12
+{
+    // Reverse Number
+    internal class Problem12
+    {
+
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+
+            return Num;
+
+        }
+
+        static int ReverseNumber(int Number)
+        {
+
+            int Remainder = 0, Number2 = 0;
+            while (Number > 0)
+            {
+
+                Remainder = Number % 10;
+                Number /= 10;
+                Number2 = Number2 * 10 + Remainder;
+
+            }
+
+            return Number2;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Reverse Number is :\n" +
+                              ReverseNumber(ReadPositiveNumbers("Entre a Positive Number?")));
+
+        }
+    }
+}
