4b1cce7f1c2868187974fb774b6c7b21c57e9589
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-13 02:07:26 +0200

    Print Number Pattern

---
+++ Problem13/Problem13.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem13
+{
+    // Print Number Pattern
+    internal class Problem13
+    {
+
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+            } while (Num <= 0);
+            return Num;
+
+        }
+
+        static void PrintNumberpattern(int Num)
+        {
+
+            for (int i = 1; i <= Num; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write(i);
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintNumberpattern(ReadPositiveNumbers("Enter a Positive Number?"));
+
+        }
+    }
+}
