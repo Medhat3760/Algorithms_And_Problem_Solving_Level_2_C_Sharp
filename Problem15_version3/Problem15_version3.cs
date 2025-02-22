15b80ccc14a709e5744e98e5351d2d7f76550e4a
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-22 07:09:28 +0200

    Print Letter Pattern

---
+++ Problem15_version3/Problem15_version3.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem15_version3
+{
+    // Print Letter Pattern
+    internal class Problem15_version3
+    {
+
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+            } while (Num <= 0);
+            return Num;
+
+        }
+
+        static void PrintLetterPattern(int Number)
+        {
+
+            for (int i = 1; i <= Number; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write((char)(64 + i));
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintLetterPattern(ReadPositiveNumbers("Enter a number?"));
+
+        }
+    }
+}
