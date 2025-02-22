882d24499503f84b92a201db0b94259336e3b120
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-22 06:52:19 +0200

    Print Inverted Letter Pattern

---
+++ Problem14_version3/Problem14_version3.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem14_version3
+{
+    // Print Inverted Letter Pattern
+    internal class Problem14_version3
+    {
+
+        static int ReadPositiveNumbers(string Message)
+        {
+
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+
+            return Num;
+
+        }
+
+        static void PrintInvertedLetterPattern(int Number)
+        {
+
+            for (int i = Number; i >= 1; i--)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write((char)(64 + i));
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintInvertedLetterPattern(ReadPositiveNumbers("Enter a number?"));
+
+        }
+    }
+}
