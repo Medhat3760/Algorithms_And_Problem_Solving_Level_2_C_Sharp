0d6f71659091bf157fe9eeba7b65a9e100c96c27
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-23 02:17:32 +0200

    Print Words From AAA to ZZZ

---
+++ Problem16_version2/Problem16_version2.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem16_version2
+{
+    // Print Words From AAA to ZZZ
+    internal class Problem16_version2
+    {
+
+        static void PrintWordsFromAAAtoZZZ()
+        {
+
+            string Word = "";
+
+            for (int i = 65; i <= 90; i++)
+            {
+
+                for (int j = 65; j <= 90; j++)
+                {
+
+                    for (int k = 65; k <= 90; k++)
+                    {
+
+                        Word += (char)i;
+                        Word += (char)j;
+                        Word += (char)k;
+
+                        Console.WriteLine(Word);
+
+                        Word = "";
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            PrintWordsFromAAAtoZZZ();
+
+        }
+    }
+}
