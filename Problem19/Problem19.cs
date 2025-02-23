aed69d7f118c1ffcd40493a8787c9a14e90c3899
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-23 03:23:41 +0200

    Random 3 Numbers From 1 to 10

---
+++ Problem19/Problem19.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem19
+{
+    // Random 3 Numbers From 1 to 10
+    internal class Problem19
+    {
+
+        static Random random = new Random();
+
+        static int RandomNumber(int From, int To)
+        {
+
+            int RandNum = random.Next(From, To + 1);
+
+            return RandNum;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(RandomNumber(1, 10));
+            Console.WriteLine(RandomNumber(1, 10));
+            Console.WriteLine(RandomNumber(1, 10));
+
+        }
+    }
+}
