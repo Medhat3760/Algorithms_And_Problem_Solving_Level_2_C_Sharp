1cb2b8b50a61b0ae110fcaa231af3e5d395aa84c
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-23 02:33:36 +0200

    Guess a 3-Letter Password

---
+++ Problem17/Problem17.cs
@@ -0,0 +1,68 @@
+﻿namespace Problem17
+{
+    // Guess a 3-Letter Password
+    internal class Problem17
+    {
+
+        static string ReadPassWord(string Message)
+        {
+
+            string Pass = "";
+            Console.WriteLine(Message);
+            Pass = Console.ReadLine();
+            return Pass;
+
+        }
+
+        static bool GuessPasswordFromAAAtoZZZ(string Pass)
+        {
+
+            Console.WriteLine();
+
+            int Counter = 0;
+            string Word = "";
+
+            for (int i = 65; i <= 90; i++)
+            {
+
+                for (int j = 65; j <= 90; j++)
+                {
+
+                    for (int k = 65; k <= 90; k++)
+                    {
+                        Counter++;
+                        Word += (char)i;
+                        Word += (char)j;
+                        Word += (char)k;
+                        Console.WriteLine("Trial [" + Counter + "]: " + Word);
+
+                        if (Word == Pass)
+                        {
+
+                            Console.WriteLine("\nPassword is " + Pass);
+                            Console.WriteLine("Found After " + Counter + " Trial(s)");
+                            return true;
+
+                        }
+                        Word = "";
+
+                    }
+                    Console.WriteLine();
+
+                }
+                Console.WriteLine();
+
+            }
+
+            return false;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            GuessPasswordFromAAAtoZZZ(ReadPassWord("Enter a 3-Letter Password (all capital)?"));
+
+        }
+    }
+}
