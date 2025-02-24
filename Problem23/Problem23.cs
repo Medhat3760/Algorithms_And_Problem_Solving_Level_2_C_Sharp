e99ceccb1926fc013d2c428488d9b803391110f9
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-24 22:32:20 +0200

    Fill Array With Random Numbers

---
+++ Problem23/Problem23.cs
@@ -0,0 +1,69 @@
+﻿namespace Problem23
+{
+    // Fill Array With Random Numbers
+    internal class Problem23
+    {
+
+        static int ReadPositiveNumber(string Message)
+        {
+
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+            return Num;
+        }
+
+        static Random random = new Random();
+
+        static int RandomNumber(int From, int To)
+        {
+
+            int RandNum = random.Next(From, To + 1);
+            return RandNum;
+
+        }
+
+        static void FillArrayWithRandomNumbers(int[] Arr, ref int Length)
+        {
+
+            Length = ReadPositiveNumber("Enter How Many items you Want To Fill?");
+
+            for (int i = 0; i < Length; i++)
+            {
+
+                Arr[i] = RandomNumber(1, 100);
+
+            }
+
+        }
+
+        static void PrintArray(int[] Arr, int Length)
+        {
+
+            for (int i = 0; i < Length; i++)
+            {
+
+                Console.Write(Arr[i] + " ");
+
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[100];
+            int Length = 0;
+            FillArrayWithRandomNumbers(arr, ref Length);
+
+            Console.Write("\nArray Elements: ");
+            PrintArray(arr, Length);
+
+        }
+    }
+}
