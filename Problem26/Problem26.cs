379d4bfe89046c7d9fe9dda852e7a70345414f1e
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-25 01:06:17 +0200

    Sum Of Random Array

---
+++ Problem26/Problem26.cs
@@ -0,0 +1,85 @@
+﻿namespace Problem26
+{
+    // Sum Of Random Array
+    internal class Problem26
+    {
+
+        static int ReadPositiveNumber(string Message)
+        {
+
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+            return Num;
+        }
+
+        static Random random = new Random();
+
+        static int RandomNumber(int From, int To)
+        {
+
+            int RandNum = random.Next(From, To + 1);
+            return RandNum;
+
+        }
+
+        static void FillArrayWithRandomNumbers(int[] Arr, ref int Length)
+        {
+
+            Length = ReadPositiveNumber("Enter How Many items you Want To Fill?");
+
+            for (int i = 0; i < Length; i++)
+            {
+
+                Arr[i] = RandomNumber(1, 100);
+
+            }
+
+        }
+
+        static void PrintArray(int[] Arr, int Length)
+        {
+
+            for (int i = 0; i < Length; i++)
+            {
+
+                Console.Write(Arr[i] + " ");
+
+            }
+            Console.WriteLine();
+        }
+
+        static int SumArray(int[] Arr, int arrLength)
+        {
+
+            int Sum = 0;
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                Sum += Arr[i];
+
+            }
+            return Sum;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[100];
+            int Length = 0;
+
+            FillArrayWithRandomNumbers(arr, ref Length);
+
+            Console.Write("\nArray Elements: ");
+            PrintArray(arr, Length);
+
+            Console.WriteLine("\nSum Of All Numbers is: " + SumArray(arr, Length));
+
+        }
+    }
+}
