56a5dcf15098c744ed362b446ec4634a99dcf44e
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-28 23:30:49 +0200

    My Round

---
+++ Problem47/Problem47.cs
@@ -0,0 +1,59 @@
+﻿namespace Problem47
+{
+    // My Round
+    internal class Problem47
+    {
+
+        static float ReadNumber(string Message)
+        {
+
+            float Num = 0;
+            Console.WriteLine(Message);
+            Num = float.Parse(Console.ReadLine());
+            return Num;
+
+        }
+
+        static float GetFractionPart(float Num)
+        {
+
+            return Num - (int)Num;
+
+        }
+
+        static int MyRound(float Num)
+        {
+
+            int IntPart = (int)Num;
+            float FractionPart = GetFractionPart(Num);
+
+            if (Math.Abs(FractionPart) >= 0.5)
+            {
+
+                if (Num > 0)
+                {
+                    return ++IntPart;
+                }
+                else
+                {
+                    return --IntPart;
+                }
+
+            }
+
+            return IntPart;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            float Number = ReadNumber("Enter a Number?");
+
+            Console.WriteLine("\n My Round Result: " + MyRound(Number));
+
+            Console.WriteLine("\n C# Round Result: " + Math.Round(Number));
+
+        }
+    }
+}
