6d5759ff76704ef24daf68727b7e65bd4966e845
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-28 23:58:27 +0200

    My Floor

---
+++ Problem48/Problem48.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem48
+{
+    // My Floor
+    internal class Problem48
+    {
+
+        static float ReadNumber(string Message)
+        {
+
+            float Num = 0;
+            Console.WriteLine(Message);
+            Num = float.Parse(Console.ReadLine());
+            return Num;
+
+        }
+
+        static int MyFloor(float Num)
+        {
+
+            if (Num > 0)
+            {
+                return (int)Num;
+            }
+
+            return (int)--Num;
+        }
+
+        static void Main(string[] args)
+        {
+
+            float Num = ReadNumber("Enter a Number?");
+
+            Console.WriteLine("\nMy Floor: " + MyFloor(Num));
+            Console.WriteLine("\nC# Floor: " + Math.Floor(Num));
+
+        }
+    }
+}
