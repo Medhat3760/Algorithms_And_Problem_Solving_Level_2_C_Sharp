ea851fc482671c6b4e3ad3776ed9fdd913255310
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-02-28 06:31:48 +0200

    Count Negative Numbers In Array

--- Problem45/Problem45.cs
+++ Problem45/Problem45.cs
@@ -1,10 +1,86 @@
-﻿namespace Problem45
+namespace Problem45
 {
-    internal class Program
+    // Count Negative Numbers In Array
+    internal class Problem45
     {
+
+        static int ReadPositiveNumber(string Message)
+        {
+            int Num;
+            do
+            {
+
+                Console.WriteLine(Message);
+                Num = int.Parse(Console.ReadLine());
+
+            } while (Num <= 0);
+
+            return Num;
+        }
+
+        static Random random = new Random();
+        static int RandomNumberInRange(int From, int To)
+        {
+
+            int randNum = random.Next(From, To + 1);
+            return randNum;
+
+        }
+
+        static void FillArrayWithRandomNumbers(int[] arr, ref int arrLength)
+        {
+
+            arrLength = ReadPositiveNumber("Enter number of elements?");
+
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                arr[i] = RandomNumberInRange(-100, 100);
+
+            }
+
+        }
+
+        static int NegativeNumbersCountInArray(int[] arr, int arrLength)
+        {
+            int Count = 0;
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                if (arr[i] < 0)
+                {
+                    Count++;
+                }
+
+            }
+
+            return Count;
+        }
+
+        static void PrintArray(int[] arr, int arrLength)
+        {
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int[] arr = new int[100];
+            int arrLength = 0;
+
+            FillArrayWithRandomNumbers(arr, ref arrLength);
+
+            Console.WriteLine("\nArray Elements: ");
+            PrintArray(arr, arrLength);
+
+            Console.WriteLine("\nNegative Numbers Count is: " + NegativeNumbersCountInArray(arr, arrLength));
+
         }
     }
 }
