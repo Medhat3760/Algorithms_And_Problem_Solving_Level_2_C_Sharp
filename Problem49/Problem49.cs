d6c3c90834651541dedf42850b1870d96ba50b3b
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-03-01 02:27:50 +0200

    My Ceil

---
+++ Problem49/Problem49.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem49
+{
+    // My Ceil
+    internal class Problem49
+    {
+
+        static float ReadNumber(string Message)
+        {
+
+            float Num = 0;
+            Console.WriteLine(Message);
+            Num = float.Parse(Console.ReadLine());
+            return Num;
+
+        }
+
+        static int MyCeil(float Num)
+        {
+
+            float FractionPart = Num - (int)Num;
+
+            if (FractionPart != 0 && Num > 0)
+            {
+                return (int)Num + 1;
+            }
+
+            return (int)Num;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            float Num = ReadNumber("Enter a Number?");
+
+            Console.WriteLine("\nMy Ceil: " + MyCeil(Num));
+            Console.WriteLine("\nC# Ceil: " + Math.Ceiling(Num));
+
+        }
+    }
+}
