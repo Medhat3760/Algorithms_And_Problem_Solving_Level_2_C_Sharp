612a46ec975cc684cfd3a1905246ca7959308fd7
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-03-01 03:23:34 +0200

    My Sqrt

---
+++ Problem50/Problem50.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem50
+{
+    // My Sqrt
+    internal class Problem50
+    {
+
+        static float ReadNumber(string Message)
+        {
+
+            float Num = 0;
+            Console.WriteLine(Message);
+            Num = float.Parse(Console.ReadLine());
+            return Num;
+
+        }
+
+        static float MySqrt(float Num)
+        {
+
+            return (float)Math.Pow(Num, 0.5);
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            float Num = ReadNumber("Enter a Number?");
+
+            Console.WriteLine("\nMySqrt Result: " + MySqrt(Num));
+
+            Console.WriteLine("\nC# Sqrt Result: " + Math.Sqrt(Num));
+
+        }
+    }
+}
